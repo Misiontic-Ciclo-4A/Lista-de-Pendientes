5cea72dad4b6c81f7e23f26ba4dab385ab88422a
demarchenac <demarchenac@uninorte.edu.co>
2022-08-27 03:46:38 -0500

    Sesión 07: Cargar solución

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import "./App.css";
+import { useState } from "react";
 import { TodoList } from "./components/TodoList";
+import "./App.css";
+
+const initialTodos = [
+  { id: 1, description: "Hacer la compra", isDone: false },
+  { id: 2, description: "Terminar la tarea", isDone: false },
+  { id: 3, description: "Pasear a Milo", isDone: false },
+];
 
 export default function App() {
+  const [todos, setTodos] = useState(initialTodos);
+
+  const updateTodo = (id) => {
+    const newTodos = [...todos];
+    const todoToToggle = newTodos.findIndex((todo) => todo.id === id);
+    newTodos[todoToToggle].isDone = !newTodos[todoToToggle].isDone;
+    setTodos(newTodos);
+  };
+
   return (
     <div className="App">
       <h1>Lista de Pendientes</h1>
-      <TodoList />
+      <TodoList todos={todos} onToggle={updateTodo} />
     </div>
   );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,13 +1,24 @@
 import { TodoItem } from "./TodoItem";
 
-export function TodoList() {
+export function TodoList({ onToggle, todos = [] }) {
+  if (todos.length === 0) {
+    return <span>No hay ning&uacute;n pendiente...</span>;
+  }
+  const incomplete = todos.filter(({ isDone }) => !isDone);
+
   return (
     <>
-      <span>Pendientes: 03.</span>
+      <span> Pendientes: {incomplete.length}. </span>
       <ul className="card">
-        <TodoItem description="Ver la sesión #06" isDone />
-        <TodoItem description="Hacer el componente práctico" />
-        <TodoItem description="Guardar cámbios" />
+        {todos?.map(({ id, description, isDone }) => (
+          <TodoItem
+            key={id}
+            id={id}
+            description={description}
+            isDone={isDone}
+            onToggle={onToggle}
+          />
+        ))}
       </ul>
     </>
   );
