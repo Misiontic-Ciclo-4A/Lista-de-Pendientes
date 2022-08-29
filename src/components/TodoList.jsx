6c2150b9ac7b8025a50dd9e89c03495b76fbdf12
demarchenac <demarchenac@uninorte.edu.co>
2022-08-29 02:07:44 -0500

    Minor changes

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,8 +3,10 @@ import { TodoItem } from "./TodoItem";
 export function TodoList() {
   return (
     <>
+      <hr />
       <span>Pendientes: 03.</span>
-      <ul className="card">
+      <hr />
+      <ul>
         <TodoItem description="Ver la sesión #06" isDone />
         <TodoItem description="Hacer el componente práctico" />
         <TodoItem description="Guardar cámbios" />
